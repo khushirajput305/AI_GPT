dce45bdb19479f45cc78d946611ab9fb4b229b19
khushirajput305 <khushinurawat305@gmail.com>
2025-01-27 12:14:48 +0530

    first commit

---
+++ Backend/index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const dotenv = require("dotenv");
+const morgan = require("morgan");
+const cors = require("cors")
+const bodyParser = require("body-parser")
+const colors = require("colors");
+const connectDB = require("./config/db");
+
+//routes path
+const authRoutes = require('./routes/authRoutes');
+const errorHandler = require("./middlewares/errorMiddleware");
+
+//dotenv
+dotenv.config();
+
+//mongodb conncetion
+connectDB();
+//rest object
+const app = express();
+
+//middlewares
+app.use(cors())
+app.use(express.json())
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(morgan("dev"))
+app.use(errorHandler)
+
+const PORT = process.env.PORT || 8080;
+
+//api routes
+app.use('/api/v1/auth',authRoutes)
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`.bgCyan.white);
+});
